3cf60e7b0e9c668f95c00c5513330b683509d90c
Beeeemac <Tomorrowdead2222@gmail.com>
2017-02-17 00:02:41 -0500

    Update app.js

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ var config = {
   // - For your faucet to work, you must register your site at Recaptcha
   // - https://www.google.com/recaptcha/intro/index.html
   recaptcha_sitekey: '6Ldq6BUUAAAAAPtTvXuiKD_9W2v4S88cM27YQ4Oi',  // <----- EDIT ME!
-  redirect_uri: 'https://BEEEEMAC.GITHUB.IO.github.io',
+  redirect_uri: 'https://beeeemac.github.io',
   mp_browser_uri: 'https://www.moneypot.com',
   mp_api_uri: 'https://api.moneypot.com',
   chat_uri: '//socket.moneypot.com',
