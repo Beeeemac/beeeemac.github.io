5c93ccbefc0ffef50b13e3ba05bf4dfc5a8f6495
Beeeemac <Tomorrowdead2222@gmail.com>
2017-02-17 01:45:49 -0500

    Update app.js

--- app.js
+++ app.js
@@ -4,13 +4,13 @@
 
 var config = {
   // - Your app's id on moneypot.com
-  app_id: 18,                             // <----------------------------- EDIT ME!
+  app_id: 2682,                             // <----------------------------- EDIT ME!
   // - Displayed in the navbar
-  app_name: 'untitled-dice.github.io',
+  app_name: 'beeeemac.github.io',
   // - For your faucet to work, you must register your site at Recaptcha
   // - https://www.google.com/recaptcha/intro/index.html
   recaptcha_sitekey: '6Ldq6BUUAAAAAPtTvXuiKD_9W2v4S88cM27YQ4Oi',  // <----- EDIT ME!
-  redirect_uri: 'https://untitled-dice.github.io',
+  redirect_uri: 'https://beeeemac.github.io',
   mp_browser_uri: 'https://www.moneypot.com',
   mp_api_uri: 'https://api.moneypot.com',
   chat_uri: '//socket.moneypot.com',
